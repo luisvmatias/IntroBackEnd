f3d6699fcb57c239e76910d27ae6cf5376a3bbd8
Luís Matias <luisvictormatias7@gmail.com>
2025-08-29 21:32:28 -0300

    aula pratica dia 29/08

---
+++ praticas/02- calculadora/CalculadoraNota.test.js
@@ -0,0 +1,13 @@
+let {calcularNotaA1} = require('./CalculadoraNota')
+let {describe, expect, test} = require('@jest/globals')
+
+describe ('Testando Modulo Calcular Nota' , () => {
+    
+    test('calcularNotaA1 -> ex 1, trb 3, prov 6 = 10', () => {
+        expect(calcularNotaA1(1,3,6)).toBe(10)
+    })
+    test('calcularNotaA1 -> ex 0, trb 0, prov 0 = 0', () => {
+        expect(calcularNotaA1(0,0,0)).toBe(0)
+    })
+    
+})
